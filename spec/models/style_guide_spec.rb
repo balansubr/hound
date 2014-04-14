d927af77d04a2ef78af60f28d87eda57542e7cf1
Greg Lazarev <greg@thoughtbot.com>
2014-04-13 23:31:37 -0700

    Add default style guide test coverage

--- spec/models/style_guide_spec.rb
+++ spec/models/style_guide_spec.rb
@@ -100,6 +100,118 @@ describe StyleGuide, '#violations' do
         ]
       end
     end
+
+    describe 'inline comments' do
+      it 'does not have a violation' do
+        pending
+        expect(violations_in('def foo # bad method')).to eq [
+          'Avoid inline comments'
+        ]
+      end
+    end
+
+    describe 'comma white space' do
+      it 'does not have violation' do
+        expect(violations_in('def foobar(a, b, c)')).to be_empty
+      end
+
+      it 'has violation' do
+        expect(violations_in('def foobar(a,b,c)')).to eq [
+          'Space missing after comma.',
+          'Space missing after comma.'
+        ]
+      end
+    end
+
+    describe 'semicolon white space' do
+      it 'does not have violation' do
+        expect(violations_in('class foo; bar; end')).to be_empty
+      end
+
+      it 'has violation' do
+        expect(violations_in('class foo;bar; end')).to eq [
+          'Space missing after semicolon.'
+        ]
+      end
+    end
+
+    describe 'colon white space' do
+      it 'does not have violation' do
+        expect(violations_in('admin? ? true : false')).to be_empty
+      end
+
+      it 'has violation' do
+        expect(violations_in('admin? ? true: false')).to eq [
+          "Surrounding space missing for operator ':'."
+        ]
+      end
+    end
+
+    describe 'multiline method chaining' do
+      it 'does not have violation' do
+        content = <<-CONTENT.strip_heredoc.gsub(/\n\Z/)
+        foo.
+          bar.
+          baz
+        CONTENT
+        puts violations_in(content)
+        expect(violations_in(content)).to be_empty
+      end
+
+      it 'has violation' do
+        pending
+        expect(violations_in("foo\n.bar\n.baz")).to eq [
+          'For multiline method invocations, place the . at the end of each line'
+        ]
+      end
+    end
+
+    describe 'empty line between methods' do
+      it 'does not have violation' do
+        content = <<-CONTENT.strip_heredoc.gsub(/\n\Z/)
+        def foo
+          bar
+        end
+
+        def bar
+          foo
+        end
+        CONTENT
+        expect(violations_in(content)).to be_empty
+      end
+
+      it 'has violation' do
+        expect(violations_in("def foo\n  bar\nend\ndef bar\n  foo\nend")).
+          to eq ['Use empty lines between defs.']
+      end
+    end
+
+    describe 'use new lines around multiline blocks' do
+      it 'does not have violation' do
+        expect(violations_in('things.each do\n  stuff\nend\n\nmore code')).
+          to be_empty
+      end
+
+      it 'has violation' do
+        pending
+        expect(violations_in('things.each do\n  stuff\nend\nmore code')).to eq [
+          'Use newlines around multi-line blocks'
+        ]
+      end
+    end
+
+    describe 'case for SQL statements' do
+      it 'does not have violation' do
+        expect(violations_in("SELECT * FROM 'users'")).to be_empty
+      end
+
+      it 'has violation' do
+        pending
+        expect(violations_in("select * FROM 'users'")).to eq [
+          'Use uppercase for SQL key words and lowercase for SQL identifiers.'
+        ]
+      end
+    end
   end
 
   private
